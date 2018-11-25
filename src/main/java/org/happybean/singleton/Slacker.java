a9cd64319bc793ad848d11bfc6036d50fa075ab6
bean <18222997200@163.com>
2018-11-25 16:37:49 +0800

    单例模式

---
+++ src/main/java/org/happybean/singleton/Slacker.java
@@ -0,0 +1,26 @@
+package org.happybean.singleton;
+
+/**
+ * @author wgt
+ * @date 2018-11-25
+ * @description 懒汉模式
+ **/
+public class Slacker {
+
+    private Slacker(){
+
+    }
+
+    private static Slacker slacker;
+
+    public static Slacker getInstance() {
+        if(slacker==null){
+            slacker = new Slacker();
+        }
+        return slacker;
+    }
+
+    public void call() {
+        System.out.println("i am slacker");
+    }
+}
